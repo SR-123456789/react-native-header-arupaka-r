6a0a53b8052aab0c704db3a2292119d5d3021b0f
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-11-05 18:25:15 +0900

    feat: header完成

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,21 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import {
-  SafeAreaProvider,
-  SafeAreaView,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 type HeaderProps = {
-  title: string;
-  style: any;
+  title?: string;
+  style?: any;
+  height?: number;
   backgroundColor: string;
+  scrollRef?: React.RefObject<any>;
   content?: () => JSX.Element;
 };
 
 const defaultHeight = 50;
-const defaultBackgroundColor = 'white';
+const defaultBackgroundColor = 'red';
 
 // HeaderContentコンポーネント（実際のヘッダー内容）
 const HeaderContent: React.FC<HeaderProps> = ({
@@ -23,21 +21,22 @@ const HeaderContent: React.FC<HeaderProps> = ({
   content,
   style,
   backgroundColor,
+  height,
 }) => {
-  const insets = useSafeAreaInsets(); // SafeAreaInsetsを取得
-  console.log(insets);
-
   return (
     <View
-      style={{
-        backgroundColor: backgroundColor
-          ? backgroundColor
-          : defaultBackgroundColor,
-        minHeight: defaultHeight,
-        width: Dimensions.get('window').width,
-      }}
+      style={[
+        styles.header,
+        {
+          backgroundColor: backgroundColor
+            ? backgroundColor
+            : defaultBackgroundColor,
+          height: height ? height : defaultHeight,
+          width: Dimensions.get('window').width,
+        },
+      ]}
     >
-      <Text style={{ ...style }}>{title}</Text>
+      <Text style={[styles.text, style]}>{title}</Text>
       {content && content()}
     </View>
   );
@@ -46,13 +45,18 @@ const HeaderContent: React.FC<HeaderProps> = ({
 // SafeAreaProviderでラップするHeaderコンポーネント
 export const Header: React.FC<HeaderProps> = (props) => {
   return (
-    <View style={{ zIndex: 1000 }}>
+    <View
+      style={{
+        zIndex: 0,
+        minHeight: props.height ? props.height : defaultHeight,
+      }}
+    >
       <SafeAreaProvider>
         <SafeAreaView
           style={{
             backgroundColor: props.backgroundColor
               ? props.backgroundColor
-              : 'red',
+              : defaultBackgroundColor,
           }}
         >
           <HeaderContent {...props} />
@@ -61,3 +65,15 @@ export const Header: React.FC<HeaderProps> = (props) => {
     </View>
   );
 };
+
+// スタイルシートの定義
+const styles = StyleSheet.create({
+  header: {
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
